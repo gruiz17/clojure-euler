2fb4622538b658a1be1634fb8d7ca2f448308842
gruiz17 <gruiz17@berkeley.edu>
2013-04-16 02:42:59 -0700

    added comments to Problem 1 solution

--- euler1.clj
+++ euler1.clj
@@ -1,10 +1,12 @@
-(defn euler-1 [n sum]
-  (if (= n 1000) (println sum)
+(defn euler-1 [n sum] ; starting this out
+  (if (= n 1000) ; if the iterator n is equal to 1000, print the calculated sum at that point
+    (println sum)
 
-  (if (or (zero? (rem n 3))
-      (zero? (rem n 5)))
-        (euler-1 (+ n 1) (+ sum n))
-      (euler-1 (+ n 1) sum))))
+  (if (or (zero? (rem n 3)) ; otherwise, check this condition
+      (zero? (rem n 5))) ; if either n%3 = 0 or n%5 = 0, run the method again recursively,
+        (euler-1 (+ n 1) (+ sum n)) ; but add one to iterator, and add whatever n was to sum
 
-(euler-1 0 0)
+      (euler-1 (+ n 1) sum)))) ; if not, just go on as usual, adding one to n
+
+(euler-1 0 0) ; call the function
 
