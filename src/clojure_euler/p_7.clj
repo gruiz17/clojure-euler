8ab491349480ad4896f9ef98ee00041b0c11b3b7
gruiz17 <gruiz17@berkeley.edu>
2014-03-24 16:38:21 -0700

    lol

--- src/clojure_euler/p_7.clj
+++ src/clojure_euler/p_7.clj
@@ -1,26 +1,2 @@
-; to determine if a number is prime
-; determine if it is divisible by any number before it
-
-; -> determine if it is divisible by any number before the number/2
-; -> determine if it is divisible by any number before sqrt(number)
 (ns p-7
   (:require [utilities.utilities]))
-(defn prime? [n]
-   (if (= 2 n) true
-     (if
-       (or
-         (even? n) false
-         (= 1 n) false
-         (= 0 n) false
-        )
-      )
-       (loop [i 3]
-        (if (> i (int (Math/sqrt n))) true
-         (if (zero? (rem n i)) false
-          (recur (+ i 2))
-         )
-        )
-       )
-      )
-    )
-  )
