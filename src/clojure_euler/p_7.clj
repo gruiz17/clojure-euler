26a497d2d42cff544e86ca248b5c4874a95b5476
gruiz17 <gruiz17@berkeley.edu>
2014-03-24 17:07:19 -0700

    proper p-7 solution

--- src/clojure_euler/p_7.clj
+++ src/clojure_euler/p_7.clj
@@ -1,2 +1,13 @@
 (ns p-7
   (:require [utilities.utilities]))
+
+(defn main []
+(def i 1)
+(def infinity 2)
+
+(while (< i 10001)
+  (def infinity (+ infinity 1))
+  (if (utilities/prime? infinity)
+    (def i (+ i 1))))
+
+(println infinity))
